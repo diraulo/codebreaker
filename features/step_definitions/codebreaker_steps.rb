13db995e03d84e485f990ddbb0b576e0e7e2e8d7
Raoul DIFFOUO <diraulo@gmail.com>
2015-02-26 05:27:15 +0200

    all green - implement 'submit guess' feature

--- features/step_definitions/codebreaker_steps.rb
+++ features/step_definitions/codebreaker_steps.rb
@@ -1,16 +1,16 @@
-# class Output
-#   def messages
-#     @messages ||= []
-#   end
+class TerminalOutput
+  def messages
+    @messages ||= []
+  end
 
-#   def puts(message)
-#     messages << message
-#   end
-# end
+  def puts(message)
+    messages << message
+  end
+end
 
-# def term_output
-#   @output ||= Output.new
-# end
+def term_output
+  @output ||= TerminalOutput.new
+end
 
 Given /^I am not yet playing$/ do
 end
@@ -18,10 +18,23 @@ end
 When /^I start a new game$/ do
   @messenger = StringIO.new
   game = Codebreaker::Game.new(@messenger)
-  game.start
+  game.start '1234'
 end
 
 Then /^I should see "(.*?)"$/ do |message|
   # expect(term_output.messages).to include(message)
   expect(@messenger.string.split("\n")).to include(message)
+end
+
+Given /^the secret code is "(.*?)"$/ do |secret|
+  @game = Codebreaker::Game.new(term_output)
+  @game.start(secret)
+end
+
+When /^I guess "(.*?)"$/ do |guess|
+  @game.guess(guess)
+end
+
+Then /^the mark should be "(.*?)"$/ do |mark|
+  expect(term_output.messages).to include(mark)
 end
